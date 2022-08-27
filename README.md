55bd4a979e0f9323537bc99aebaf793625d493cc
Fabrizio <fabrinucci99@gmail.com>
2022-08-26 21:52:39 -0300

    html and css created & api trending consumed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # project-movie-db-platzi
+
+documentationTMDB = https://developers.themoviedb.org/3/getting-started/introduction
+
+API_KEY=508a25f671b0a06c9ab8aec35944749e
+
+API_URL_EXAMPLE=https://api.themoviedb.org/3/movie/550?api_key=508a25f671b0a06c9ab8aec35944749e
--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
-console.log('Hi friend');
+const trendingSelector = document.querySelector('#trending');
+
+const API_KEY ='508a25f671b0a06c9ab8aec35944749e';
+const API_URL=`https://api.themoviedb.org/3`;
+
+/* https://api.themoviedb.org/3/trending/movie/week?api_key=508a25f671b0a06c9ab8aec35944749e  */
+
+const createDiv = (options) => {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+
+  div.setAttribute('id', `${options.id}`);
+  div.setAttribute('class', `${options.class}`);
+
+  img.setAttribute('src', `${options.img_url}`);
+  img.setAttribute('alt', `${options.title}`);
+
+  div.append(img);
+
+  return div;
+
+}
+
+const getTrending = async () => {
+
+  const res = await fetch(`${API_URL}/trending/tv/day?api_key=${API_KEY}`);
+  const data = await res.json();
+  const movies = data.results;
+
+  const fragment = new DocumentFragment;
+
+  movies.forEach( movie => {
+    const newDiv = createDiv({
+      id: movie.id,
+      class: 'display-movie',
+      img_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+      title: movie.title
+    })
+    fragment.append(newDiv)
+  })
+  trendingSelector.append(fragment);
+
+  // id,
+  // title,
+  // overview,
+  // vote_average,
+
+
+  console.log(movies);
+}
+
+getTrending();
